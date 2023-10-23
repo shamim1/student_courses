75f494c29346bd08b5212370a22145df67d7be75
Ashraful Alam <Ashraul.alam1@gmail.com>
2023-10-23 10:24:16 -0300

    Initial commit

---
+++ api/src/test/java/com/dematic/coding/ashraful/studentapi/StudentapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dematic.coding.ashraful.studentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
